5348cb7f372a482adae0567c0fae704d1509025d
kzajac111 <krzysztofzajac111@gmail.com>
2019-10-26 13:28:39 +0200

    Add empty solving method for genetic algorithm

---
+++ OAST.Project1.Services/Genetic/GeneticService.cs
@@ -0,0 +1,13 @@
+﻿using OAST.Project1.Models.Genetic;
+using System.Threading.Tasks;
+
+namespace OAST.Project1.Services.Genetic
+{
+    public class GeneticService : IGeneticService
+    {
+        public async Task SolveAllocationProblem(GeneticAlgorithmParameters parameters)
+        {
+
+        }
+    }
+}
---
+++ OAST.Project1.Services/Genetic/IGeneticService.cs
@@ -0,0 +1,10 @@
+﻿using OAST.Project1.Models.Genetic;
+using System.Threading.Tasks;
+
+namespace OAST.Project1.Services.Genetic
+{
+    public interface IGeneticService
+    {
+        Task SolveAllocationProblem(GeneticAlgorithmParameters parameters);
+    }
+}

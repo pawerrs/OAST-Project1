4948dd02f9374007c9647970cc5ea3a2e8c4d155
kzajac111 <krzysztofzajac111@gmail.com>
2019-10-26 13:12:12 +0200

    Add genetic algorithm criteria

---
+++ OAST.Project1.Models/Genetic/GeneticAlgorithmParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace OAST.Project1.Models.Genetic
+{
+    public class GeneticAlgorithmParameters
+    {
+        public int InitialPopulationSize { get; set; }
+
+        public float MutationProbability { get; set; }
+
+        public float CrossoverProbability { get; set; }
+
+        public StoppingCriteria StoppingCriteria { get; set; }
+    }
+}
---
+++ OAST.Project1.Models/Genetic/StoppingCriteria.cs
@@ -0,0 +1,10 @@
+﻿namespace OAST.Project1.Models.Genetic
+{
+    public enum StoppingCriteria
+    {
+        ElapsedTime,
+        NumberOfGenerations,
+        NumberOfMutations,
+        NoImprovement
+    }
+}

ae974c69553958bbf7b0c2fa955eefa16de57ebf
kzajac111 <krzysztofzajac111@gmail.com>
2019-10-26 12:53:49 +0200

    Add controllers for problems resolving

---
+++ OAST.Project1/Controllers/BruteForceController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OAST.Project1.Controllers
+{
+    [Route("api/bruteForce")]
+    public class BruteForceController : Controller
+    {
+    }
+}
---
+++ OAST.Project1/Controllers/GeneticController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OAST.Project1.Controllers
+{
+    [Route("api/genetic")]
+    public class GeneticController : Controller
+    {
+    }
+}

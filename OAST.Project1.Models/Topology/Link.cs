4263729ba5dc698cb68e6d55273c000bebb6e61d
showmeyourlens <adam.e.krupa@gmail.com>
2019-11-09 23:42:10 +0100

    Brute force

--- OAST.Project1.Models/Topology/Link.cs
+++ OAST.Project1.Models/Topology/Link.cs
@@ -7,6 +7,8 @@ namespace OAST.Project1.Models.Topology
         public int EndNode { get; set; }
         public int NumberOfModules { get; set; }
         public double ModuleCost { get; set; }
-        public int LinkModule { get; set; } 
+        public int CapacityPerModule { get; set; } 
+        public int TotalLoad { get; set; }
+        public int SignalsCount { get; set; }
     }
 }
---
+++ OAST.Project1.Services/BruteForce/DemandDistributions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OAST.Project1.Services.BruteForce
+{
+    public class DemandDistributions
+    {
+        public int demandId { get; private set; }
+        public List<List<int>> distributions { get; private set; }
+
+        public DemandDistributions(int demandId)
+        {
+            this.demandId = demandId;
+        }
+
+        private void Subsets(int n, int k, List<int> chosen)
+        {
+            if (k == 0)
+            {
+                PrepareResult(chosen);
+                return;
+            }
+            if (n == 0)
+            {
+                return;
+            }
+            Subsets(n - 1, k, new List<int>(chosen));
+            chosen.Insert(0, n);
+            Subsets(n - 1, k - 1, new List<int>(chosen));
+        }
+
+        private void ReturnResult(List<int> data)
+        {
+            distributions.Add(data);
+        }
+
+        private void PrepareResult(List<int> data)
+        {
+            data.Add(9);
+            ReturnResult(new List<int>(data.Select(x => x - (data.IndexOf(x) > 0 ? data[data.IndexOf(x) - 1] : 0) - 1).ToList()));
+            data.RemoveAt(data.Count - 1);
+        }
+
+        public void FindAllDistributions(int distributionVolume, int numberOfPaths)
+        {
+            List<int> chosen = new List<int>();
+            Subsets(distributionVolume + numberOfPaths - 1, numberOfPaths - 1, chosen);
+        }
+    }
+}

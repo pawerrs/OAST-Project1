46dce5a290ad0c4ea80372febd418710a174ebe7
kzajac111 <krzysztofzajac111@gmail.com>
2019-11-10 15:19:50 +0100

    Genetic algorithm initial implementation

---
+++ OAST.Project1.Models/Genetic/GeneticAlgorithmState.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace OAST.Project1.Models.Genetic
+{
+    public class GeneticAlgorithmState
+    {
+        public Stopwatch ElapsedTime { get; set; }
+
+        public int NumberOfGenerations { get; set; }
+
+        public int NumberOfMutations { get; set; }
+
+        public int NumberOfGenerationsWithoutImprovement { get; set; }
+    }
+}
--- OAST.Project1.Services/Genetic/GeneticService.cs
+++ OAST.Project1.Services/Genetic/GeneticService.cs
@@ -1,28 +1,65 @@
-﻿using OAST.Project1.Models.Genetic;
+﻿using System;
+using System.Diagnostics;
+using OAST.Project1.Models.Genetic;
 using System.Threading.Tasks;
+using OAST.Project1.Common.Enums;
 using OAST.Project1.DataAccess.ConfigHandlers;
+using OAST.Project1.Models.Common;
 
 namespace OAST.Project1.Services.Genetic
 {
-    public class GeneticService : IGeneticService
+    public class GeneticService
     {
         private readonly FileParser _fileParser;
+        private readonly FileName _fileName;
+        private readonly ProblemType _problemType;
+        private readonly GeneticAlgorithmParameters _parameters;
 
-        public GeneticService()
+        public GeneticService(MenuOptions menuOptions)
         {
+            _parameters = menuOptions.GeneticAlgorithmParameters;
+            _fileName = menuOptions.FileName;
+            _problemType = menuOptions.ProblemType;
+
             _fileParser = new FileParser();
         }
 
-        public async Task SolveDAP(GeneticAlgorithmParameters parameters)
+        public void Solve()
         {
-            //var network = _fileParser.LoadTopology(FileReader.ReadFile(@"P:\STUFF\studia\mgr\sem2\OAST\OAST.Project1.DataAccess\Input Data\net12_1"));
+            Action fitnessFunction = null;
 
-            
+            switch (_problemType)
+            {
+                case ProblemType.DDAP:
+                    fitnessFunction = EvaluateDDAPSolution;
+                    break;
+                case ProblemType.DAP:
+                    fitnessFunction = EvaluateDAPSolution;
+                    break;
+            }
+
+            OptimizeWithGeneticAlgorithm(fitnessFunction);
         }
 
-        public async Task SolveDDAP(GeneticAlgorithmParameters parameters)
+        private void OptimizeWithGeneticAlgorithm(Action fitnessFunction)
         {
-           // var network = _fileParser.LoadTopology(FileReader.ReadFile(@"P:\STUFF\studia\mgr\sem2\OAST\OAST.Project1.DataAccess\Input Data\net12_1"));
+            var status = new GeneticAlgorithmState
+            {
+                ElapsedTime = Stopwatch.StartNew(),
+                NumberOfGenerations = 0,
+                NumberOfGenerationsWithoutImprovement = 0,
+                NumberOfMutations = 0
+            };
+        }
+
+        private void EvaluateDDAPSolution()
+        {
+
+
+        }
+
+        private void EvaluateDAPSolution()
+        {
 
         }
 
@@ -30,5 +67,15 @@ namespace OAST.Project1.Services.Genetic
         {
 
         }
+
+        private bool EvaluateStoppingCriteria(GeneticAlgorithmState state) =>
+            _parameters.StoppingCriteria switch
+            {
+                StoppingCriteria.ElapsedTime => state.ElapsedTime.ElapsedMilliseconds / 1000 >= _parameters.LimitValue,
+                StoppingCriteria.NoImprovement => state.NumberOfGenerationsWithoutImprovement >= _parameters.LimitValue,
+                StoppingCriteria.NumberOfGenerations => state.NumberOfGenerations >= _parameters.LimitValue,
+                StoppingCriteria.NumberOfMutations => state.NumberOfGenerations >= _parameters.LimitValue,
+                _ => false
+            };
     }
 }
